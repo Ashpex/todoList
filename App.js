7a367488bd69064e7f8f0f9bb9007de79cd29e98
ashpex <ashpex@pm.me>
2021-07-26 13:58:14 +0700

    add basic add task and remove task feature

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from './components/Task'
 
 export default function App() {
+
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () =>  {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+const completeTask = (index) => {
+  let itemsCopy = [...taskItems];
+  itemsCopy.splice(index,1);
+  setTaskItems(itemsCopy);
+}
+
   return (
     <View style={styles.container}>
       {/* Today's Task */}
@@ -11,10 +27,34 @@ export default function App() {
 
         <View style={styles.items}>
           {/* Task list is placed here! */}
-          <Task text={'Task 1'} />
-          <Task text={'Task 2'} />
+          {
+            taskItems.map((item, index) => {
+              return(
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item} />
+                
+                </TouchableOpacity>
+              ) 
+            })
+          }
+
         </View>
       </View>
+
+      {/*Write a task*/}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'android' ? "padding": "height"} 
+        style={styles.writeTaskWrapper}
+      >
+
+        <TextInput style={styles.input} placeholder={"Write a task "} value ={task} onChangeText={text => setTask(text)} />
+
+        <TouchableOpacity onPress = {() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -35,4 +75,39 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30,
   },
+  
+  writeTaskWrapper:{
+    position:'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250
+  },
+
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+
+  addText: {
+
+  },
+
 });
